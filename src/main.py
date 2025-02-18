26df591fb6dbacd611020aa491bb564340354e6f
audiomagicks <audiomagicks@gmail.com>
2025-02-18 12:50:51 -0600

    refactor non-class functions into mytools

--- src/main.py
+++ src/main.py
@@ -1,27 +1,13 @@
 from textnode import *
 from htmlnode import *
+from mytools import *
 
 
 def main():
     test_node = TextNode('test test', TextType.IMAGE, 'boot.dev')
     print(text_node_html_node(test_node))
 
-def text_node_html_node(text_node):
-    match text_node.text_type:
-        case TextType.TEXT:
-            return LeafNode(None, text_node.text, None)
-        case TextType.BOLD:
-            return LeafNode('b', text_node.text, None)
-        case TextType.ITALIC:
-            return LeafNode('i', text_node.text, None)
-        case TextType.CODE:
-            return LeafNode('code', text_node.text, None)
-        case TextType.LINK:
-            return LeafNode('a', text_node.text, {'href':text_node.url})
-        case TextType.IMAGE:
-            return LeafNode('img', '', {'src':text_node.url, 'alt':text_node.text})
-        case _:
-            raise Exception('invalid type')
+
         
 
 main()
